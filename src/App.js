b8cf8e7c9b8e31c569656492f9e151aab06f6be3
m-woo <mark.chi.woo@gmail.com>
2017-02-11 17:30:13 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import $ from 'jquery';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+          <h1>{2*2}</h1>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          <input></input>
+          <button onClick={() =>this.search()}>Catch 'em</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+var pokeSearch = null
+
+$.get('http://pokeapi.co/api/v2/'+ pokeSearch,{
+
+/ })
+search = () => {
+  console.log('search')
+};
+
+
+
+
+export default App;
